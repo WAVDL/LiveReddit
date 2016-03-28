b622c08c915a6a32f8c9ec5c697e116711868369
CKalnasy <kalnasy.6@osu.edu>
2016-03-28 12:19:19 -0400

    updated threads list row views

--- app/src/main/java/edu/osu/livereddit/ThreadsListActivity.java
+++ app/src/main/java/edu/osu/livereddit/ThreadsListActivity.java
@@ -1,24 +1,28 @@
 package edu.osu.livereddit;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.models.Listing;
 import net.dean.jraw.models.Submission;
 import net.dean.jraw.paginators.SubredditPaginator;
-import net.dean.jraw.paginators.UserSubredditsPaginator;
+
+import java.util.Date;
 
 public class ThreadsListActivity extends AppCompatActivity {
     private RedditClient redditClient = GlobalVars.getRedditClient();
     private String subredditName;
-    private Listing<Submission> submissions;
+    private Submission[] submissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +30,39 @@ public class ThreadsListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_threads_list);
 
         subredditName = getIntent().getStringExtra("subreddit_name");
+
+        setTitle(subredditName);
+
         ThreadsListTask threadsListTask = new ThreadsListTask();
         threadsListTask.execute((Void) null);
     }
 
     private void success() {
-        if (!submissions.isEmpty()) {
-            String[] list = new String[25];
-            for (int i = 0; i < submissions.size(); i++) {
-                list[i] = submissions.get(i).getTitle();
+        ThreadsArrayAdapter adapter = new ThreadsArrayAdapter(this, submissions);
+        ListView listView = (ListView) findViewById(R.id.threads_list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent = new Intent(ThreadsListActivity.this, ThreadActivity.class);
+//                intent.putExtra("thread_full_name", submissions[position].getFullName());
+//                startActivity(intent);
             }
-
-            ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.threads_listview, list);
-            ListView listView = (ListView) findViewById(R.id.threads_list);
-            listView.setAdapter(adapter);
-
-//            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                @Override
-//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                    Intent intent = new Intent(SubredditsList.this, ThreadsListActivity.class);
-//                    intent.putExtra("subreddit_name", submissions.get(position).getDisplayName());
-//                    startActivity(intent);
-//                }
-//            });
-        }
+        });
     }
 
     public class ThreadsListTask extends AsyncTask<Void, Void, Boolean> {
         @Override
         protected Boolean doInBackground(Void... params) {
-            System.out.println(redditClient.isAuthenticated());
             SubredditPaginator subredditPaginator = new SubredditPaginator(redditClient, subredditName);
-            submissions = subredditPaginator.next();
+            Listing<Submission> list = subredditPaginator.next();
+
+            submissions = new Submission[list.size()];
+            for (int i = 0; i < submissions.length; i++) {
+                submissions[i] = list.get(i);
+            }
+
             return true;
         }
 
@@ -66,6 +71,49 @@ public class ThreadsListActivity extends AppCompatActivity {
             if (success) {
                 success();
             }
+        }
+    }
+
+    public class ThreadsArrayAdapter extends ArrayAdapter {
+        private final Context context;
+        private final Submission[] values;
+
+        public ThreadsArrayAdapter(Context context, Submission[] values) {
+            super(context, -1, values);
+            this.context = context;
+            this.values = values;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.threads_listview, parent, false);
+
+            Submission submission = values[position];
+
+            TextView scoreTextView = (TextView) rowView.findViewById(R.id.score);
+            scoreTextView.setText(submission.getScore() + "");
+
+            TextView titleTextView = (TextView) rowView.findViewById(R.id.title);
+            titleTextView.setText(submission.getTitle());
+
+            String submissionTime = formatTime(submission.getCreatedUtc());
+            TextView detailsTextView = (TextView) rowView.findViewById(R.id.details);
+            detailsTextView.setText(submission.getCommentCount() + " Comments • " + submissionTime + " • u/" + submission.getAuthor());
+
+            return rowView;
+        }
+
+        private String formatTime(Date date) {
+            Date now = new Date();
+            long minutes = (now.getTime() - date.getTime()) / 1000 / 60;
+            if (minutes < 60) {
+                return minutes + "m";
+            } else if (minutes < 60 * 24) {
+                return minutes / 60 + "h";
+            }
+            return minutes / 60 / 24 + "d";
         }
     }
 }
